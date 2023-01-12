691fd12e50c5c90251646e60d559aafd95a3b5cd
ttaehee <ttaehee1105@gmail.com>
2023-01-12 10:55:38 +0900

    Init : project 환경설정
    - JPA 환경설정
    - DB 설정 암호화(외부 환경파일로 분리)
    - .gitignore
    - dependency
    - Logback

---
+++ src/main/java/com/prgrms/kream/ShoeKreamApplication.java
@@ -0,0 +1,13 @@
+package com.prgrms.kream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoeKreamApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShoeKreamApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/prgrms/kream/ShoeKreamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prgrms.kream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoeKreamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
