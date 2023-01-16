c93db736b122174d8263fbc72643a5d2194b5905
ttaehee <ttaehee1105@gmail.com>
2023-01-16 20:16:02 +0900

    Feat : [SHK-99] Image Entity 구현

--- src/main/java/com/prgrms/kream/domain/image/model/Image.java
+++ src/main/java/com/prgrms/kream/domain/image/model/Image.java
@@ -1,4 +1,51 @@
 package com.prgrms.kream.domain.image.model;
 
-public class Image {
+import static lombok.AccessLevel.*;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.prgrms.kream.common.model.BaseTimeEntity;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "IMAGE")
+@NoArgsConstructor(access = PROTECTED)
+public class Image extends BaseTimeEntity {
+
+	@Id
+	@Column(name = "id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(name = "original_name", nullable = false)
+	private String originalName;
+
+	@Column(name = "unique_name", nullable = false)
+	private String fullPath;
+
+	@Column(name = "reference_id", nullable = false)
+	private Long referenceId;
+
+	@Enumerated(EnumType.STRING)
+	@Column(name = "domain_type", nullable = false)
+	private DomainType domainType;
+
+	@Builder
+	public Image(String originalName, String fullPath, Long referenceId, DomainType domainType) {
+		this.originalName = originalName;
+		this.fullPath = fullPath;
+		this.referenceId = referenceId;
+		this.domainType = domainType;
+	}
 }
