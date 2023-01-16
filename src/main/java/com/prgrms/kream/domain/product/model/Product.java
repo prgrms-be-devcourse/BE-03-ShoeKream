b445428e40233b7b37ab8bfa0f3882360a191fb4
ttaehee <ttaehee1105@gmail.com>
2023-01-16 20:40:58 +0900

    Feat : [SHK-98] Product Entity 구현

--- src/main/java/com/prgrms/kream/domain/product/model/Product.java
+++ src/main/java/com/prgrms/kream/domain/product/model/Product.java
@@ -1,4 +1,44 @@
 package com.prgrms.kream.domain.product.model;
 
-public class Product {
+import static lombok.AccessLevel.*;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.prgrms.kream.common.model.BaseTimeEntity;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "PRODUCT")
+@NoArgsConstructor(access = PROTECTED)
+public class Product extends BaseTimeEntity {
+
+	@Id
+	@Column(name = "id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(name = "name", nullable = false)
+	private String name;
+
+	@Column(name = "release_price", nullable = false)
+	private int releasePrice;
+
+	@Column(name = "description", nullable = false)
+	private String description;
+
+	@Builder
+	public Product(String name, int releasePrice, String description) {
+		this.name = name;
+		this.releasePrice = releasePrice;
+		this.description = description;
+	}
 }
