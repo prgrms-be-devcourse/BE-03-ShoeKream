41ecf476494a0829f616106ca49d99707440f68c
ttaehee <ttaehee1105@gmail.com>
2023-01-18 01:20:00 +0900

    Feat : [SHK-94] Product 조회 기능 구현

--- src/main/java/com/prgrms/kream/domain/product/facade/ProductFacade.java
+++ src/main/java/com/prgrms/kream/domain/product/facade/ProductFacade.java
@@ -2,13 +2,17 @@ package com.prgrms.kream.domain.product.facade;
 
 import static com.prgrms.kream.common.mapper.ProductMapper.*;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.prgrms.kream.domain.image.model.DomainType;
 import com.prgrms.kream.domain.image.service.ImageService;
+import com.prgrms.kream.domain.product.controller.dto.ProductGetResponse;
 import com.prgrms.kream.domain.product.controller.dto.ProductRegisterRequest;
 import com.prgrms.kream.domain.product.controller.dto.ProductRegisterResponse;
+import com.prgrms.kream.domain.product.facade.dto.ProductDto;
 import com.prgrms.kream.domain.product.service.ProductService;
 
 import lombok.RequiredArgsConstructor;
@@ -26,5 +30,12 @@ public class ProductFacade {
 				= productService.register(toProductRegisterDto(productRegisterRequest));
 		imageService.register(productRegisterRequest.images(), productRegisterResponse.id(), DomainType.PRODUCT);
 		return productRegisterResponse;
+	}
+
+	@Transactional(readOnly = true)
+	public ProductGetResponse get(Long productId) {
+		ProductDto productDto = productService.get(productId);
+		List<String> imagePaths = imageService.getAll(productId, DomainType.PRODUCT);
+		return toProductGetResponse(productDto, imagePaths);
 	}
 }
