a794cd5d09b5be8dc91c339cc4de99ab0b714567
bufflect <bndgc@naver.com>
2023-01-18 18:40:11 +0900

    Test : [SHK-124] Spring Security 기본 Filter 설정

--- src/main/java/com/prgrms/kream/common/config/SecurityConfig.java
+++ src/main/java/com/prgrms/kream/common/config/SecurityConfig.java
@@ -1,4 +1,29 @@
 package com.prgrms.kream.common.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
 public class SecurityConfig {
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http
+				.authorizeRequests()
+				.anyRequest().permitAll()
+				.and()
+				.csrf().disable()
+				.headers().disable()
+				.formLogin().disable()
+				.httpBasic().disable()
+				.rememberMe().disable()
+				.logout().disable()
+				.sessionManagement()
+				.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+		;
+
+		return http.build();
+	}
 }
