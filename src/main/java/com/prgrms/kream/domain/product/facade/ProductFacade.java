154654fff8d839467d00fb0eb61f6676ce577283
ttaehee <ttaehee1105@gmail.com>
2023-01-24 21:56:49 +0900

    Feat : [SHK-147] Product 목록 캐싱 기능 구현
    - 상품 목록 caching
    - 상품 등록 시, cache 삭제

--- src/main/java/com/prgrms/kream/domain/product/facade/ProductFacade.java
+++ src/main/java/com/prgrms/kream/domain/product/facade/ProductFacade.java
@@ -4,6 +4,8 @@ import static com.prgrms.kream.common.mapper.ProductMapper.*;
 
 import java.util.List;
 
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -26,6 +28,7 @@ public class ProductFacade {
 	private final ProductService productService;
 	private final ImageService imageService;
 
+	@CacheEvict(value = "products", allEntries = true)
 	@Transactional
 	public ProductRegisterResponse register(ProductRegisterRequest productRegisterRequest) {
 		ProductRegisterResponse productRegisterResponse
@@ -41,6 +44,7 @@ public class ProductFacade {
 		return toProductGetResponse(productGetFacadeResponse, imagePaths);
 	}
 
+	@Cacheable(cacheNames = "products", key = "#productGetAllRequest")
 	@Transactional(readOnly = true)
 	public ProductGetAllResponses getAll(ProductGetAllRequest productGetAllRequest) {
 		return productService.getAll(productGetAllRequest);
