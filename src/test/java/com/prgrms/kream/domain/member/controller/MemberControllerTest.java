ad7f610a06057493c4f8aec25f4e54171f8d3759
bufflect <bndgc@naver.com>
2023-01-25 16:46:31 +0900

    Test : [SHK-153] 로그인 기능 테스트

--- src/test/java/com/prgrms/kream/domain/member/controller/MemberControllerTest.java
+++ src/test/java/com/prgrms/kream/domain/member/controller/MemberControllerTest.java
@@ -6,18 +6,22 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.prgrms.kream.MysqlTestContainer;
+import com.prgrms.kream.domain.member.dto.request.MemberLoginRequest;
 import com.prgrms.kream.domain.member.dto.request.MemberRegisterRequest;
+import com.prgrms.kream.domain.member.model.Member;
+import com.prgrms.kream.domain.member.repository.MemberRepository;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -28,14 +32,32 @@ class MemberControllerTest extends MysqlTestContainer {
 	private MockMvc mockMvc;
 
 	@Autowired
-	private Jackson2ObjectMapperBuilder jackson2ObjectMapperBuilder;
+	private ObjectMapper objectMapper;
+
+	@Autowired
+	private MemberRepository memberRepository;
+
+	@BeforeEach
+	void setUp() {
+		memberRepository.save(
+				Member.builder()
+						.email("hello@naver.com")
+						.name("name")
+						.password("Pa!12345678")
+						.phone("01012345678")
+						.isMale(true)
+						.authority(ROLE_USER)
+						.build());
+	}
+
+	@AfterEach
+	void tearDown() {
+		memberRepository.deleteAll();
+	}
 
 	@Test
-	@DisplayName("회원가입 controller test - 성공")
+	@DisplayName("회원가입 - 성공")
 	void register_success() throws Exception {
-
-		ObjectMapper objectMapper = jackson2ObjectMapperBuilder.build();
-
 		MemberRegisterRequest memberRegisterRequest = new MemberRegisterRequest(
 				"name", "email@naver.com", "01012345678", "aA12345678!", true, ROLE_USER);
 
@@ -46,5 +68,35 @@ class MemberControllerTest extends MysqlTestContainer {
 				.andExpect(jsonPath("$.data.id").value(1L))
 				.andDo(print())
 		;
+	}
+
+	@Test
+	@DisplayName("로그인 - 성공")
+	void login_success() throws Exception {
+		MemberLoginRequest memberLoginRequest = new MemberLoginRequest(
+				"hello@naver.com", "Pa!12345678"
+		);
+
+		mockMvc.perform(post("/api/v1/member/login")
+						.contentType(APPLICATION_JSON)
+						.content(objectMapper.writeValueAsString(memberLoginRequest))
+				).andExpect(status().isOk())
+				.andExpect(header().exists("access_token"))
+				.andDo(print())
+				.andReturn();
+	}
+
+	@Test
+	@DisplayName("로그인 - 실패 비밀번호 불일치")
+	void login_fail() throws Exception {
+		MemberLoginRequest memberLoginRequest = new MemberLoginRequest(
+				"hello@naver.com", "P!12345678"
+		);
+
+		mockMvc.perform(post("/api/v1/member/login")
+						.contentType(APPLICATION_JSON)
+						.content(objectMapper.writeValueAsString(memberLoginRequest))
+				).andExpect(status().is4xxClientError())
+				.andDo(print());
 	}
 }
--- src/test/java/com/prgrms/kream/domain/member/service/MemberServiceTest.java
+++ src/test/java/com/prgrms/kream/domain/member/service/MemberServiceTest.java
@@ -3,14 +3,21 @@ package com.prgrms.kream.domain.member.service;
 import static com.prgrms.kream.domain.member.model.Authority.*;
 import static org.mockito.Mockito.*;
 
+import java.util.Optional;
+
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.authentication.BadCredentialsException;
 
+import com.prgrms.kream.common.jwt.Jwt;
+import com.prgrms.kream.domain.member.dto.request.MemberLoginRequest;
 import com.prgrms.kream.domain.member.dto.request.MemberRegisterRequest;
+import com.prgrms.kream.domain.member.dto.response.MemberLoginResponse;
 import com.prgrms.kream.domain.member.dto.response.MemberRegisterResponse;
 import com.prgrms.kream.domain.member.model.Member;
 import com.prgrms.kream.domain.member.repository.MemberRepository;
@@ -20,6 +27,9 @@ class MemberServiceTest {
 
 	@InjectMocks
 	private MemberService memberService;
+
+	@Mock
+	private Jwt jwt;
 
 	@Mock
 	private MemberRepository memberRepository;
@@ -38,12 +48,66 @@ class MemberServiceTest {
 				.build();
 
 		MemberRegisterRequest memberRegisterRequest = new MemberRegisterRequest(
-				"name", "email@naver.com", "01012345678", "aA12345678!", true, ROLE_USER);
+				"name", "email@naver.com", "01012345678", "aA12345678!", true, ROLE_USER
+		);
 
 		when(memberRepository.save(any(Member.class)))
 				.thenReturn(member);
 
 		MemberRegisterResponse register = memberService.register(memberRegisterRequest);
 		verify(memberRepository, times(1)).save(any(Member.class));
+	}
+
+	@Test
+	@DisplayName("로그인 실패 - 비밀번호 불일치")
+	void login_success() {
+		String password = "Pa!12345678";
+		Member member = Member.builder()
+				.id(1L)
+				.email("hello@naver.com")
+				.password(password)
+				.name("hello")
+				.phone("01012345678")
+				.authority(ROLE_USER)
+				.isMale(true)
+				.build();
+
+		when(memberRepository.findByEmail(member.getEmail()))
+				.thenReturn(Optional.of(member));
+
+		MemberLoginRequest memberLoginRequest = new MemberLoginRequest(member.getEmail(), "wrongPassword");
+		Assertions.assertThatThrownBy(() -> memberService.login(memberLoginRequest))
+				.isInstanceOf(BadCredentialsException.class);
+
+		verify(memberRepository, times(1)).findByEmail(member.getEmail());
+		verify(jwt, times(0)).sign(any(Jwt.Claims.class));
+	}
+
+	@Test
+	@DisplayName("로그인 성공")
+	void login_fail() {
+		String password = "Pa!12345678";
+		Member member = Member.builder()
+				.id(1L)
+				.email("hello@naver.com")
+				.password(password)
+				.name("hello")
+				.phone("01012345678")
+				.authority(ROLE_USER)
+				.isMale(true)
+				.build();
+
+		when(memberRepository.findByEmail(member.getEmail()))
+				.thenReturn(Optional.of(member));
+
+		String accessToken = "access_token";
+		when(jwt.sign(any(Jwt.Claims.class))).thenReturn(accessToken);
+
+		MemberLoginRequest memberLoginRequest = new MemberLoginRequest(member.getEmail(), password);
+		MemberLoginResponse login = memberService.login(memberLoginRequest);
+
+		Assertions.assertThat(login.token()).isEqualTo(accessToken);
+		verify(memberRepository, times(1)).findByEmail(member.getEmail());
+		verify(jwt, times(1)).sign(any(Jwt.Claims.class));
 	}
 }
