e9f2a84ddabf00c5e677a6892da30e8900e3e0ec
Taehee Kim <ttaehee1105@gmail.com>
2023-01-25 13:10:46 +0900

    [SHK-145] Product 캐싱 기능 구현 (#17)
    * Chore : [SHK-146] Caffeine cache 설정
    
    - Caffeine cache configuration class
    - cache type을 enum으로 관리
    
    * Feat : [SHK-147] Product 목록 캐싱 기능 구현
    
    - 상품 목록 caching
    - 상품 등록 시, cache 삭제
    
    * Feat : [SHK-148] Product 상세 정보 캐싱 기능 구현

---
+++ src/main/java/com/prgrms/kream/common/config/CacheType.java
@@ -0,0 +1,20 @@
+package com.prgrms.kream.common.config;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum CacheType {
+	PRODUCT("product", ConstantConfig.DEFAULT_TTL_SEC, ConstantConfig.DEFAULT_MAX_SIZE),
+	PRODUCTS("products", ConstantConfig.DEFAULT_TTL_SEC, ConstantConfig.DEFAULT_MAX_SIZE);
+
+	private final String cacheName;
+	private final int expiredAfterWrite;
+	private final int maximumSize;
+
+	static class ConstantConfig {
+		static final int DEFAULT_TTL_SEC = 300;
+		static final int DEFAULT_MAX_SIZE = 10000;
+	}
+}
