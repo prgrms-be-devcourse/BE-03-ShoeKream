b04b0aa15b5b53665db48a68fb799fb165309477
bufflect <bndgc@naver.com>
2023-01-25 16:28:38 +0900

    Feat : [SHK-151] 로그인 기능 구현

--- src/main/java/com/prgrms/kream/domain/member/controller/MemberController.java
+++ src/main/java/com/prgrms/kream/domain/member/controller/MemberController.java
@@ -2,8 +2,10 @@ package com.prgrms.kream.domain.member.controller;
 
 import static org.springframework.http.HttpStatus.*;
 
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.prgrms.kream.common.api.ApiResponse;
+import com.prgrms.kream.domain.member.dto.request.MemberLoginRequest;
 import com.prgrms.kream.domain.member.dto.request.MemberRegisterRequest;
 import com.prgrms.kream.domain.member.dto.response.MemberRegisterResponse;
 import com.prgrms.kream.domain.member.facade.MemberFacade;
@@ -24,11 +27,24 @@ public class MemberController {
 
 	private final MemberFacade memberFacade;
 
+	@Value("${jwt.accessToken}")
+	private String accessToken;
+
 	@PostMapping
 	@ResponseStatus(CREATED)
 	public ApiResponse<MemberRegisterResponse> register(
 			@RequestBody @Valid MemberRegisterRequest memberRegisterRequest
 	) {
 		return ApiResponse.of(memberFacade.register(memberRegisterRequest));
+	}
+
+	@PostMapping("/login")
+	@ResponseStatus(OK)
+	public ApiResponse<String> login(
+			@RequestBody @Valid MemberLoginRequest memberLoginRequest,
+			HttpServletResponse httpServletResponse
+	) {
+		httpServletResponse.addHeader(accessToken, memberFacade.login(memberLoginRequest).token());
+		return ApiResponse.of("로그인 성공하였습니다.");
 	}
 }
---
+++ src/main/java/com/prgrms/kream/domain/member/dto/response/MemberLoginResponse.java
@@ -0,0 +1,6 @@
+package com.prgrms.kream.domain.member.dto.response;
+
+public record MemberLoginResponse(
+		String token
+) {
+}
