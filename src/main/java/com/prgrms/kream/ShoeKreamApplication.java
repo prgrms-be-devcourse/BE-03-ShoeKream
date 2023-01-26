ace13489c734ab135822829e71d5d65748ab179d
ttaehee <ttaehee1105@gmail.com>
2023-01-26 19:21:44 +0900

    Feat : [SHK-170] AWS S3 연동해서 이미지 저장 기능 구현
    - 기존에 사용하던 ImageLocalService는 profile 설정

--- src/main/java/com/prgrms/kream/ShoeKreamApplication.java
+++ src/main/java/com/prgrms/kream/ShoeKreamApplication.java
@@ -3,7 +3,9 @@ package com.prgrms.kream;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(
+		exclude = org.springframework.cloud.aws.autoconfigure.context.ContextInstanceDataAutoConfiguration.class
+)
 public class ShoeKreamApplication {
 
 	public static void main(String[] args) {
