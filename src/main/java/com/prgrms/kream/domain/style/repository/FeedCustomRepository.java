eda03c58a4d3c7715d45a9f3d284224ddeb5f1ef
김창규 <kimc980106@naver.com>
2023-01-27 12:29:55 +0900

    Refactor : 피드 사용자 기준 조회 파라미터명 구체화
    - Member 식별자에 대해 id -> memberId로 구체화

--- src/main/java/com/prgrms/kream/domain/style/repository/FeedCustomRepository.java
+++ src/main/java/com/prgrms/kream/domain/style/repository/FeedCustomRepository.java
@@ -1,6 +1,5 @@
 package com.prgrms.kream.domain.style.repository;
 
-import java.awt.print.Pageable;
 import java.util.List;
 
 import com.prgrms.kream.domain.style.model.Feed;
@@ -9,7 +8,7 @@ public interface FeedCustomRepository {
 
 	List<Feed> findAllByTag(String tag, Long cursorId, int pageSize);
 
-	List<Feed> findAllByMember(Long id, Long cursorId, int pageSize);
+	List<Feed> findAllByMember(Long memberId, Long cursorId, int pageSize);
 
 	List<Feed> findAllOrderByCreatedAtDesc(Long cursorId, int pageSize);
 
