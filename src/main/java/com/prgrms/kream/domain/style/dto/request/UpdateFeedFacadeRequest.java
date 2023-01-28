1a01baaa9d99c779a65c034920ca9911aa39d959
김창규 <kimc980106@naver.com>
2023-01-28 22:24:58 +0900

    Feat : [SHK-178] 피드 상품태그 수정 기능 추가 구현
    1. feed_product 테이블에서 기존 feedId 기준 데이터 삭제
    2. 입력받은 상품 식별자 리스트 기준으로 새로 삽입

--- src/main/java/com/prgrms/kream/domain/style/dto/request/UpdateFeedFacadeRequest.java
+++ src/main/java/com/prgrms/kream/domain/style/dto/request/UpdateFeedFacadeRequest.java
@@ -1,6 +1,9 @@
 package com.prgrms.kream.domain.style.dto.request;
 
+import java.util.List;
+
 public record UpdateFeedFacadeRequest(
-		String content
+		String content,
+		List<Long> productIds
 ) {
 }
