f3d897210889c230a16617c1a12e94422389b9d3
ttaehee <ttaehee1105@gmail.com>
2023-01-30 21:13:15 +0900

    Refactor : [SHK-221] Image 등록 쿼리 수정
    - JdbcTemplate 사용

--- src/main/java/com/prgrms/kream/domain/image/repository/ImageRepository.java
+++ src/main/java/com/prgrms/kream/domain/image/repository/ImageRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.prgrms.kream.domain.image.model.DomainType;
 import com.prgrms.kream.domain.image.model.Image;
 
-public interface ImageRepository extends JpaRepository<Image, Long> {
+public interface ImageRepository extends JpaRepository<Image, Long>, ImageCustomRepository {
 	List<Image> findAllByReferenceIdAndDomainType(Long referenceId, DomainType domainType);
 
 	void deleteAllByReferenceIdAndDomainType(Long referenceId, DomainType domainType);
