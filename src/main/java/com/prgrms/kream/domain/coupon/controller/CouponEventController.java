37a1ebdf50cc8a7cb2f47647eec86c4067d28219
goseungwon <knmy0925@gmail.com>
2023-02-02 17:53:05 +0900

    Feat : [SHK-185] 선착순 쿠폰 관리 서비스 문서화

--- src/main/java/com/prgrms/kream/domain/coupon/controller/CouponEventController.java
+++ src/main/java/com/prgrms/kream/domain/coupon/controller/CouponEventController.java
@@ -15,11 +15,15 @@ import com.prgrms.kream.common.api.ApiResponse;
 import com.prgrms.kream.domain.coupon.dto.request.CouponEventRegisterRequest;
 import com.prgrms.kream.domain.coupon.facade.CouponFacade;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/coupons")
+@Api(tags = {"쿠폰 이벤트 컨트롤러"})
 public class CouponEventController {
 	private final CouponFacade couponFacade;
 
@@ -32,7 +36,9 @@ public class CouponEventController {
 	 */
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
+	@ApiOperation(value = "쿠폰 발급")
 	public ApiResponse<Long> applyCouponEvent(
+			@ApiParam(value = "발급할 쿠폰과 사용자 요청 정보", required = true)
 			@RequestBody @Valid CouponEventRegisterRequest couponEventRegisterRequest
 	) {
 		long queueSize = couponFacade.applyCountEvent(couponEventRegisterRequest);
@@ -46,6 +52,7 @@ public class CouponEventController {
 	 */
 	@GetMapping
 	@ResponseStatus(HttpStatus.OK)
+	@ApiOperation(value = "쿠폰 이벤트 페이지 요청")
 	public ModelAndView showCouponEventPage() {
 		return new ModelAndView("static_coupon_event_page");
 	}
