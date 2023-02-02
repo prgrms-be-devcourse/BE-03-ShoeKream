a3dddfa466a334983bd51c4fe115e0546d41bacb
knmy0925@gmail.com <85796588+goseungwon@users.noreply.github.com>
2023-02-02 16:06:55 +0900

    [SHK-31] 사용자는 자신의 대기열 순위를 확인하고 싶다. (들어갔을 때 순위) (#49)
    * Feat : [SHK-42] 이벤트 요청시 사용자가 요청 순서를 리턴한다.
    - 쿠폰 발급시 amount 가 0 초과하면 1 감소
    - amount 가 0 이하인치 확인하는 메서드 생성
    
    * Test : [SHK-181] 이벤트 요청시 사용자가 요청 순서를 리턴 테스트
    - 쿠폰 개수 테스트 및 상태 테스트
    
    * Refactor : properties, CouponEventRegisterRequest 수정
    - properties setter lombok이 아닌 직접 지정
    - CouponEventRegisterRequest 필드 Long type으로 변경

--- src/main/java/com/prgrms/kream/domain/coupon/dto/request/CouponEventRegisterRequest.java
+++ src/main/java/com/prgrms/kream/domain/coupon/dto/request/CouponEventRegisterRequest.java
@@ -1,11 +1,14 @@
 package com.prgrms.kream.domain.coupon.dto.request;
 
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
 
 public record CouponEventRegisterRequest(
+		@Positive(message = "잘못된 쿠폰 Id")
 		@NotNull(message = "쿠폰은 필수 입력사항 입니다.")
-		long couponId,
+		Long couponId,
+		@Positive(message = "잘못된 사용자 Id")
 		@NotNull(message = "멤버는 필수 입력사항 입니다.")
-		long memberId
+		Long memberId
 ) {
 }
