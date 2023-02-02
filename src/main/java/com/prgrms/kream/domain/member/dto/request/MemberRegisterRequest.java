1665aac4a104de885f3bd06349027a5cf553319e
bufflect <bndgc@naver.com>
2023-02-03 03:56:00 +0900

    Refactor : [SHK-291] bean validation 누락된 message 작성

--- src/main/java/com/prgrms/kream/domain/member/dto/request/MemberRegisterRequest.java
+++ src/main/java/com/prgrms/kream/domain/member/dto/request/MemberRegisterRequest.java
@@ -9,20 +9,21 @@ import org.hibernate.validator.constraints.Length;
 import com.prgrms.kream.domain.member.model.Authority;
 
 public record MemberRegisterRequest(
-		@NotBlank
-		@Length(max = 20)
+		@NotBlank(message = "name은 빈값일 수 없습니다.")
+		@Length(max = 20, message = "길이는 20자 이하여야 합니다.")
 		String name,
-		@Email
-		@NotBlank
+
+		@NotBlank(message = "email은 빈값일 수 없습니다.")
+		@Email(message = "이메일 형식에 맞지 않습니다.")
 		@Length(max = 20)
 		String email,
-		@NotBlank
+		@NotBlank(message = "phone은 빈값일 수 없습니다.")
 		@Length(min = 11, max = 11)
 		String phone,
-		@NotBlank
+		@NotBlank(message = "password는 빈값일 수 없습니다.")
 		@Length(min = 8, max = 16)
 		String password,
-		@NotNull
+		@NotNull(message = "gender는 빈값일 수 없습니다.")
 		Boolean isMale,
 		@NotNull
 		Authority authority
