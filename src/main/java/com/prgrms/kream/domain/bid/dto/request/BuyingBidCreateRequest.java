a6bc4bc1c664ca22242b0b2bec15ea0171cb01f3
boompatron <grapeflavoredapple@gmail.com>
2023-02-03 14:36:18 +0900

    Feat : 문서화 누락 정보 추가

--- src/main/java/com/prgrms/kream/domain/bid/dto/request/BuyingBidCreateRequest.java
+++ src/main/java/com/prgrms/kream/domain/bid/dto/request/BuyingBidCreateRequest.java
@@ -8,19 +8,19 @@ import javax.validation.constraints.Positive;
 
 @ApiModel("구매 입찰 생성 요청")
 public record BuyingBidCreateRequest(
-		@ApiModelProperty(value = "구매 입찰 id", example = "1")
+		@ApiModelProperty(value = "구매 입찰 id", required = true, example = "1")
 		@NotNull(message = "ID는 필수적으로 있어야 합니다")
 		Long id,
 
-		@ApiModelProperty(value = "구매 입찰 생성 요청자 id", example = "2")
+		@ApiModelProperty(value = "구매 입찰 생성 요청자 id", required = true, example = "2")
 		@NotNull(message = "구매 입찰을 올린 사람의 ID는 필수적으로 필요합니다")
 		Long memberId,
 
-		@ApiModelProperty(value = "상품 사이즈 id", example = "3")
+		@ApiModelProperty(value = "상품 사이즈 id", required = true, example = "3")
 		@NotNull(message = "상품의 사이즈는 필수 입력값입니다")
 		Long productOptionId,
 
-		@ApiModelProperty(value = "가격", example = "45000")
+		@ApiModelProperty(value = "가격", required = true, example = "45000")
 		@NotNull(message = "상품의 가격은 필수 입력값입니다")
 		@Positive(message = "가격은 0월 보다 많아야 합니다")
 		int price,
