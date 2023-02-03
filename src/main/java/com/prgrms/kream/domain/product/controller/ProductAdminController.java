45a2813ce798873be9a2359725320f4e044438fe
ttaehee <ttaehee1105@gmail.com>
2023-02-03 14:43:59 +0900

    Docs : [SHK-272] controller에 swagger 문서화 작업
    - swagger 관련 학습 및 팀원들과 상의 [SHK-216]

--- src/main/java/com/prgrms/kream/domain/product/controller/ProductAdminController.java
+++ src/main/java/com/prgrms/kream/domain/product/controller/ProductAdminController.java
@@ -19,17 +19,31 @@ import com.prgrms.kream.domain.product.dto.response.ProductRegisterResponse;
 import com.prgrms.kream.domain.product.dto.response.ProductUpdateResponse;
 import com.prgrms.kream.domain.product.facade.ProductFacade;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/admin/products")
+@Api(tags = "상품 관리자 컨트롤러")
 public class ProductAdminController {
 
 	private final ProductFacade productFacade;
 
 	@PostMapping
 	@ResponseStatus(code = HttpStatus.CREATED)
+	@ApiOperation(value = "상품 등록")
+	@ApiImplicitParams({
+			@ApiImplicitParam(name = "name", value = "상품 이름", required = true),
+			@ApiImplicitParam(name = "releasePrice", value = "상품 출시가격", required = true),
+			@ApiImplicitParam(name = "description", value = "상품 설명", required = true),
+			@ApiImplicitParam(name = "sizes", value = "상품 신발 사이즈", required = true),
+			@ApiImplicitParam(name = "images", value = "상품 이미지", required = true)
+	})
 	public ApiResponse<ProductRegisterResponse> registerProduct(
 			@ModelAttribute @Valid ProductRegisterRequest productRegisterRequest) {
 
@@ -38,15 +52,26 @@ public class ProductAdminController {
 
 	@PatchMapping
 	@ResponseStatus(code = HttpStatus.OK)
+	@ApiOperation(value = "상품 수정")
+	@ApiImplicitParams({
+			@ApiImplicitParam(name = "id", value = "수정할 상품 아이디", required = true),
+			@ApiImplicitParam(name = "releasePrice", value = "상품 출시가격", required = true),
+			@ApiImplicitParam(name = "description", value = "상품 설명", required = true),
+			@ApiImplicitParam(name = "sizes", value = "상품 신발 사이즈", required = true),
+			@ApiImplicitParam(name = "images", value = "상품 이미지", required = true)
+	})
 	public ApiResponse<ProductUpdateResponse> updateProduct(
+			@ApiParam(value = "상품 수정 요청 정보", required = true)
 			@ModelAttribute @Valid ProductUpdateRequest productUpdateRequest) {
 
 		return ApiResponse.of(productFacade.updateProduct(productUpdateRequest));
 	}
 
 	@DeleteMapping("/{id}")
+	@ApiOperation(value = "상품 삭제")
 	@ResponseStatus(code = HttpStatus.OK)
-	public ApiResponse<String> deleteProduct(@PathVariable Long id) {
+	public ApiResponse<String> deleteProduct(
+			@ApiParam(value = "삭제할 상품 아이디", required = true, example = "1") @PathVariable Long id) {
 		productFacade.deleteProduct(id);
 
 		return ApiResponse.of("삭제되었습니다.");
--- src/main/java/com/prgrms/kream/domain/product/controller/ProductController.java
+++ src/main/java/com/prgrms/kream/domain/product/controller/ProductController.java
@@ -17,24 +17,32 @@ import com.prgrms.kream.domain.product.dto.response.ProductGetAllResponses;
 import com.prgrms.kream.domain.product.dto.response.ProductGetResponse;
 import com.prgrms.kream.domain.product.facade.ProductFacade;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/products")
+@Api(tags = "상품 컨트롤러")
 public class ProductController {
 
 	private final ProductFacade productFacade;
 
 	@GetMapping("/{id}")
 	@ResponseStatus(code = HttpStatus.OK)
-	public ApiResponse<ProductGetResponse> getProduct(@PathVariable Long id) {
+	@ApiOperation(value = "상품 상세 조회")
+	public ApiResponse<ProductGetResponse> getProduct(
+			@ApiParam(value = "상세 조회할 상품 아이디", required = true, example = "1") @PathVariable Long id) {
 		return ApiResponse.of(productFacade.getProduct(id));
 	}
 
 	@PostMapping
 	@ResponseStatus(code = HttpStatus.OK)
+	@ApiOperation(value = "상품 목록 조회")
 	public ApiResponse<ProductGetAllResponses> getAllProducts(
+			@ApiParam(value = "상품 목록 조회 요청 정보", required = true)
 			@RequestBody @Valid ProductGetAllRequest productGetAllRequest) {
 		return ApiResponse.of(productFacade.getAllProducts(productGetAllRequest));
 	}
