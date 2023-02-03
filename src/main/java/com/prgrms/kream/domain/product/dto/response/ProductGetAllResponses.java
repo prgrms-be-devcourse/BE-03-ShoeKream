ef04fe9412c1b369954e2d6d61dea51959d2b871
ttaehee <ttaehee1105@gmail.com>
2023-02-03 14:44:47 +0900

    Docs : [SHK-273] dto에 swagger 문서화 작업

--- src/main/java/com/prgrms/kream/domain/product/dto/response/ProductGetAllResponses.java
+++ src/main/java/com/prgrms/kream/domain/product/dto/response/ProductGetAllResponses.java
@@ -2,7 +2,14 @@ package com.prgrms.kream.domain.product.dto.response;
 
 import java.util.List;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+@ApiModel(value = "상품 목록 조회 응답 정보")
 public record ProductGetAllResponses(
+		@ApiModelProperty(value = "상품 목록")
 		List<ProductGetAllResponse> productGetAllResponses,
+
+		@ApiModelProperty(value = "상품 목록 중 마지막 상품 아이디", example = "1")
 		Long lastId) {
 }
