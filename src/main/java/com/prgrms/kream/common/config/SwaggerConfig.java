899cc584c3fbaa6feb6f8f8b470b676257f3a2c4
bufflect <bndgc@naver.com>
2023-02-04 17:22:10 +0900

    Refactor : [SHK-304] swagger 테스트를 위한 SwaggerConfig authorize 설정 추가

--- src/main/java/com/prgrms/kream/common/config/SwaggerConfig.java
+++ src/main/java/com/prgrms/kream/common/config/SwaggerConfig.java
@@ -1,6 +1,7 @@
 package com.prgrms.kream.common.config;
 
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 import org.springframework.context.annotation.Bean;
@@ -11,7 +12,11 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.ApiKey;
+import springfox.documentation.service.AuthorizationScope;
+import springfox.documentation.service.SecurityReference;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -31,11 +36,13 @@ public class SwaggerConfig {
 						.basePackage("com.prgrms.kream"))
 				.paths(PathSelectors.ant("/api/v1/**"))
 				.build()
-				.apiInfo(apiInfo());
+				.apiInfo(apiInfo())
+				.securityContexts(List.of(securityContext()))
+				.securitySchemes(List.of(apiKey()))
+				;
 	}
 
 	private ApiInfo apiInfo() {
-
 		return new ApiInfoBuilder()
 				.title("Shoe-Kream")
 				.description("명품 중고거래 플랫폼 서비스인 크림의 클론 프로젝트")
@@ -43,8 +50,28 @@ public class SwaggerConfig {
 				.build();
 	}
 
+	private ApiKey apiKey() {
+		return new ApiKey("JWT", "access_token", "header");
+	}
+
+	private SecurityContext securityContext() {
+		return SecurityContext.builder()
+				.securityReferences(defaultAuth())
+				.build();
+	}
+
+	private List<SecurityReference> defaultAuth() {
+		AuthorizationScope authorizationScope =
+				new AuthorizationScope("global", "accessEverything");
+		AuthorizationScope[] authorizationScopes =
+				new AuthorizationScope[1];
+
+		authorizationScopes[0] = authorizationScope;
+		return List.of(new SecurityReference("JWT", authorizationScopes));
+	}
+
 	private Set<String> getConsumeContentTypes() {
-		Set<String> consumes = new HashSet<>();
+		HashSet<String> consumes = new HashSet<>();
 		consumes.add(MediaType.APPLICATION_JSON_VALUE);
 		consumes.add(MediaType.MULTIPART_FORM_DATA_VALUE);
 		return consumes;
@@ -55,5 +82,4 @@ public class SwaggerConfig {
 		produces.add(MediaType.APPLICATION_JSON_VALUE);
 		return produces;
 	}
-
 }
