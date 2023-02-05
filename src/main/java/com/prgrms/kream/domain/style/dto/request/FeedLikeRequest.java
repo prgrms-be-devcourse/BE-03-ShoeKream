72e96edc304fc3285d89527f4bf4001f0ae8e016
김창규 <65993842+Kim-Changgyu@users.noreply.github.com>
2023-02-05 10:11:34 +0900

    [SHK-168] Feed API 문서화 (#65)
    * Docs : [SHK-281] Feed API 문서화 (Swagger)
    
    * Docs : [SHK-282] Feed javadoc 적용

--- src/main/java/com/prgrms/kream/domain/style/dto/request/FeedLikeRequest.java
+++ src/main/java/com/prgrms/kream/domain/style/dto/request/FeedLikeRequest.java
@@ -3,9 +3,14 @@ package com.prgrms.kream.domain.style.dto.request;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Positive;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+@ApiModel("좋아요 요청 정보")
 public record FeedLikeRequest(
 		@NotNull(message = "사용자 식별값은 비어있을 수 없습니다.")
 		@Positive(message = "사용자 식별값이 올바르지 않습니다.")
+		@ApiModelProperty(value = "사용자 식별자", required = true, example = "1")
 		Long memberId
 ) {
 }
--- src/main/java/com/prgrms/kream/domain/style/dto/response/FeedGetResponse.java
+++ src/main/java/com/prgrms/kream/domain/style/dto/response/FeedGetResponse.java
@@ -3,14 +3,33 @@ package com.prgrms.kream.domain.style.dto.response;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+
+@ApiModel("피드 응답 개별 정보")
 public record FeedGetResponse(
+		@ApiModelProperty(value = "피드 식별자", example = "1")
 		Long id,
+
+		@ApiModelProperty(value = "작성자 식별자", example = "1")
 		Long authorId,
+
+		@ApiModelProperty(value = "본문", example = "피드 본문")
 		String content,
+
+		@ApiModelProperty(value = "좋아요", example = "100")
 		Long likes,
+
+		@ApiModelProperty(value = "상품 태그", example = "[1, 2, 3]")
 		List<Long> products,
+
+		@ApiModelProperty(value = "피드 이미지 경로", example = "image.png")
 		List<String> images,
+
+		@ApiModelProperty(value = "생성시각", example = "2023-02-03T07:40:26.416Z")
 		LocalDateTime createdAt,
+
+		@ApiModelProperty(value = "수정시각", example = "2023-02-03T07:40:26.416Z")
 		LocalDateTime updatedAt
 ) {
 }
